c0270075175a4fce6b6d999258c8b6cb36535746
Alexey Dartau <iamdartau@gmail.com>
2021-03-01 21:01:56 +0600

    added methods

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,4 +1,29 @@
 package com.company;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.rmi.UnmarshalException;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+
+        Students students = new Students("newName", "NesSurname", 89);
+        DAO dao = new DaoImpl();
+        dao.addStident(students);
+
+        Scanner in = new Scanner(System.in);
+        try {
+            Socket socket = new Socket("127.0.0.1", 9889);
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            PackageData data;
+            while (true) {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/company/DAO.java
+++ src/com/company/DAO.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public interface DAO {
 
      Students getStudentByName(String name);
+     ArrayList<Students> getStudensts();
+     void addStident(Students student);
 
 }
